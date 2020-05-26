bc6717f7b5d542baa38d92440b3b82c1c3f75aee
nanako kan <n.k.0213.0529@gmail.com>
2020-05-26 18:08:01 +0900

    GraphQLの導入

--- app/graphql/types/blog_type.rb
+++ app/graphql/types/blog_type.rb
@@ -2,5 +2,5 @@ Types::BlogType = GraphQL::ObjectType.define do
   name "Blog"
   field :id, !types.ID
   field :title, !types.String
-  field :content, !types.Text
+  field :content, !types.String
 end
